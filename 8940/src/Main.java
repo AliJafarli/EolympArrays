81541c900486e9e46dc2ef6501f1d1dc84d9f2c3
AliJafarli <somagni4icent@gmail.com>
2024-10-21 15:02:47 +0400

    nice

--- 8940/src/Main.java
+++ 8940/src/Main.java
@@ -6,17 +6,16 @@ public class Main {
         int n = scanner.nextInt();
         int m = scanner.nextInt();
 
-        char[][] array = new char[n][m];
-
-        for (int i = 0; i < array.length; i++) {
-
-            for (int j = 0; j < array.length; j++) {
 
 
-                array[i][j] = '#';
-                System.out.print(array[i][j]);
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < m; j++) {
+
+                System.out.print('#');
 
             }
+            System.out.println();
 
         }
     }
