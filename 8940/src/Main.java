098d6536ed200c2a586711c63398519d18a43445
AliJafarli <somagni4icent@gmail.com>
2024-11-08 22:47:17 +0400

    nice123456789

--- 8940/src/Main.java
+++ 8940/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-         int n = scanner.nextInt();
+          int n = scanner.nextInt();
          int m = scanner.nextInt();
 
         for (int i = 0; i < n; i++) {
